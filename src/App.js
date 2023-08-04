e594f9696b7d883bc2033932c9fc146ac3e1edad
Erivaldo Sobral <erivaldo.sobral@iteris.com.br>
2023-08-04 14:59:58 -0300

    chore: adding location

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const HelloWorld = () => {
+
+  return (
+    <div>Hello World! Notlify test in {window?.location?.href ?? ''}</div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HelloWorld />
       </header>
     </div>
   );
