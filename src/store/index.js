5bd56b08c8077ba8688344948f7a3b6e825ed494
Erivaldo Sobral <erivaldo.sobral@iteris.com.br>
2023-08-05 12:39:49 -0300

    refactor

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counter from '../../src/components/Counter/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter,
-  },
-});
